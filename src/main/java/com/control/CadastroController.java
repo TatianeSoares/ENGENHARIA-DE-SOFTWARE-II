485b8e6eda947b8d3661556f6365b86a9d98af4e
pedroamlemos <peedroamlemos@gmail.com>
2022-09-30 11:00:28 -0300

    Implementacao do LoginController e CadastroController para redirecionar para o index apos realizar a operacao (https://github.com/TatianeSoares/ENGENHARIA-DE-SOFTWARE-II/issues/4) (https://github.com/TatianeSoares/ENGENHARIA-DE-SOFTWARE-II/issues/3) (https://github.com/TatianeSoares/ENGENHARIA-DE-SOFTWARE-II/issues/2)

--- src/main/java/com/control/CadastroController.java
+++ src/main/java/com/control/CadastroController.java
@@ -1,15 +1,12 @@
 package com.control;
 
 import com.business.UsuarioBusiness;
-import com.dao.UsuarioDAO;
 import com.model.Usuario;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.Serializable;
 
 @Named
@@ -28,7 +25,7 @@ public class CadastroController implements Serializable {
     public void cadastrarUsuario() throws Exception {
         UsuarioBusiness usuarioBusiness = new UsuarioBusiness();
         usuarioBusiness.cadastrarUsuario(this.usuario);
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuario cadastrado com sucesso"));
+        FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
     }
 
     @PostConstruct
