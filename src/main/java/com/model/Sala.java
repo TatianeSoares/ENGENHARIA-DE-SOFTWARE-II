32f50a029b9567d2cc2243c5f252c482ad484bfa
pedroamlemos <peedroamlemos@gmail.com>
2022-11-17 15:04:39 -0300

    inserir e remove sala

---
+++ src/main/java/com/model/Sala.java
@@ -0,0 +1,26 @@
+package com.model;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sala implements Serializable {
+    @NotEmpty(message = "O identificador de sala deve ser preenchido")
+    private String identificador;
+
+    @NotEmpty(message = "O tipo deve ser preenchido")
+    private String tipo;
+
+    @NotEmpty(message = "A capacidade deve ser preenchida")
+    private int capacidade;
+
+    @NotEmpty(message = "O departamento deve ser preenchido")
+    private String departamento;
+
+    private String infoAdicional;
+}
